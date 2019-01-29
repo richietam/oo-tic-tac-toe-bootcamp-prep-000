a97d0a6a8659020650f58ecee6bbecaba254492d
richardtam.ny <richardtam.ny@gmail.com>
2019-01-29 20:25:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,7 +84,7 @@ class TicTacToe
       if p1 == "X" && p2 == "X" && p3 == "X" || p1 == "O" && p2 == "O" && p3 == "O"
         return win_combination
       else
-        nil
+        false
       end
     end
   end
