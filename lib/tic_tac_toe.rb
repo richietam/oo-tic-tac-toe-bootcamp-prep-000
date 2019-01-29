70de89394f4f48b5a1c9bd1b42a55a4218217584
richardtam.ny <richardtam.ny@gmail.com>
2019-01-29 19:36:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,11 @@ class TicTacToe
   def position_taken?(index)
     @board[index] == "X" || @board[index] == "O"
   end
-end
+
 
   def valid_move?(index)
     position_taken?(index) == false && index.between?(0,8)
   end
+
+
+end
